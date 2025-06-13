110dd9c4d335a84e08fc028849d45701f5e58ed7
Lucca Shizuru <luccashizuru@gmail.com>
2025-06-13 16:21:33 -0300

    refactor/ alteracao da cor do fundo

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,19 @@ import About from './pages/About.jsx';
 import Projects from './pages/Projects.jsx';
 import Contact from './pages/Contact.jsx';
 import Navbar from './Components/Navbar.jsx';
-import Footer from './components/Footer.jsx';
-
 function App() {
     return (
         <Router>
             <div style={{
                 display: 'flex',
                 minHeight: '100vh',
-                backgroundColor: '#111827', // bg-gray-900
+                backgroundColor: '#0a0a0a',
                 color: 'white'
             }}>
                 <Navbar />
                 <main style={{
                     flexGrow: 1,
-                    marginLeft: '160px', // mesma largura do Navbar
+                    marginLeft: '160px',
                     padding: '24px',
                 }}>
                     <Routes>
@@ -27,7 +25,6 @@ function App() {
                         <Route path="/projetos" element={<Projects />} />
                         <Route path="/contato" element={<Contact />} />
                     </Routes>
-                    <Footer />
                 </main>
             </div>
         </Router>
